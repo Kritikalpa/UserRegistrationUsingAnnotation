f423260cf1324cf2e8b6b6573e85e4522c3a2b2e
Kriti Kalpa <krtklpsh@gmail.com>
2020-10-16 10:58:07 +0530

    [Kriti] Add. Numeric Digit condition in password

--- UserRegistrationusingAnnotation/User.cs
+++ UserRegistrationusingAnnotation/User.cs
@@ -31,7 +31,7 @@ namespace UserRegistrationusingAnnotation
 
         [Required(ErrorMessage = "Password is required")]
         [DataType(DataType.Password)]
-        [RegularExpression(@"^(?=.{8,})(?=.*[A-Z]).*$")]
+        [RegularExpression(@"^(?=.{8,})(?=.*[A-Z])(?=.*[0-9]).*$")]
         public string password { get; set; }
     }
 }
